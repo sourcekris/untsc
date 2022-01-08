f776bf38c875c70a304cb1bcbe4053e785108abf
Kris <kris@sneakerz.org>
2022-01-08 18:38:46 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+## untsc - extractor for TSComp files
+
+A proprietary format by The Stirling Group. These files 
+are DCL imploded with some basic header.
+
+#### Requires
+
+To build:
+ - golang 1.16
+
+To install binary release:
+ - See releases for builds for most major OS and architectures.
+
+#### Usage
+
+```shell
+usage: untsc [-h] -e FILENAME [-d PATH]
+
+Extract TSComp files.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -e FILENAME, --extract FILENAME
+                        The NSK file to extract.
+  -d PATH, --destination PATH
+                        An optional output folder.
+
+Please file bugs on the GitHub Issues. Thanks
+```
+
+### Format Info
+Reverse engineering of the format I found:
+
+- 6 byte file signature: "\x65\x5D\x13\x8C\x08\x01"
+- 8 bytes unknown
+- 4 byte integer - Compressed data size
+- 10 bytes unknown
+- 1 byte filename length
+- n bytes Filename (null terminated)
+- remainder - DCL Imploded payload
+
+### References
+Archive format information: http://fileformats.archiveteam.org/wiki/TSComp
+DOS Archiver: https://www.sac.sk/download/pack/tscomp.zip
+
+#### Author
+
+- Kris Hunt (@ctfkris)
---
+++ untsc.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"reflect"
+
+	"github.com/JoshVarga/blast"
+)
+
+const (
+	name       = "TSComp"
+	ext        = "TSC"
+	fHeaderLen = 13 // fileID + 7 unknown bytes
+)
+
+var (
+	fileID  = []byte{0x65, 0x5D, 0x13, 0x8C, 0x08, 0x01}
+	fset    = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	arcFile = fset.String("e", "", fmt.Sprintf("The %s file to extract.", ext))
+	dstPath = fset.String("d", "", "Optional output directory to extract to.")
+)
+
+func errpanic(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+type header struct {
+	id     []byte // Stores the file sig
+	cSize  int    // Compressed file size
+	fnSize int    // Filename length
+	fSize  int    // Total file size
+	fn     string // Filename for this header.
+}
+
+func main() {
+	fset.Parse(os.Args[1:])
+
+	if *arcFile == "" {
+		fset.Usage()
+		os.Exit(0)
+	}
+
+	if *dstPath != "" {
+		if _, err := os.Stat(*dstPath); os.IsNotExist(err) {
+			fmt.Fprintf(os.Stderr, "destination folder (%s) doesn't exist: %v", *dstPath, err)
+			os.Exit(1)
+		}
+	}
+
+	f, err := os.Open(*arcFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error opening %s: %v", *arcFile, err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	s, err := f.Stat()
+	errpanic(err)
+
+	h := &header{
+		id:    make([]byte, len(fileID)),
+		fSize: int(s.Size()),
+	}
+
+	// Read the file header.
+	_, err = f.Read(h.id)
+	errpanic(err)
+
+	if !reflect.DeepEqual(h.id, fileID) {
+		fmt.Fprintf(os.Stderr, "file is not a %s file: %v", ext, err)
+		os.Exit(1)
+	}
+
+	// See to the first entry header.
+	_, err = f.Seek(0xd, io.SeekStart)
+	errpanic(err)
+
+	// Read archive members in a loop until done.
+	for {
+		// Read metadate from file.
+		metadata := make([]byte, 16)
+		_, err = f.Read(metadata)
+		errpanic(err)
+		h.cSize = int(binary.LittleEndian.Uint32(metadata[1:5]))
+		h.fnSize = int(metadata[15])
+
+		if h.fnSize > 12 {
+			fmt.Fprintf(os.Stderr, "filename length is > 12: %d", h.fnSize)
+			os.Exit(1)
+		}
+
+		fn := make([]byte, h.fnSize)
+		_, err = f.Read(fn)
+		errpanic(err)
+		h.fn = string(fn)
+
+		fmt.Printf("Extracting: %s (%d compressed bytes)\n", h.fn, h.cSize)
+
+		_, err = f.Seek(1, io.SeekCurrent) // Seek passed the null string terminator.
+		// Read all the DCL compressed data.
+		dcl := make([]byte, h.cSize)
+		_, err = f.Read(dcl)
+		errpanic(err)
+
+		b := bytes.NewReader(dcl)
+		r, err := blast.NewReader(b)
+		errpanic(err)
+
+		if *dstPath != "" {
+			h.fn = *dstPath + "/" + h.fn
+		}
+
+		o, err := os.Create(h.fn)
+		errpanic(err)
+
+		_, err = io.Copy(o, r)
+		errpanic(err)
+		o.Close()
+
+		cur, err := f.Seek(0, io.SeekCurrent)
+		errpanic(err)
+		if cur == int64(h.fSize) {
+			break
+		}
+	}
+}
